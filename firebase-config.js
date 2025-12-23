43bb3d5780e6c30796ff7a8498a19daed8e28246
mahamudcoc5545-lang <mahamudcoc5545@gmail.com>
2025-12-23 11:38:38 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCN_MH6u2Bpo3bxfDC_dhC19U67LP8ZS_E",
+    authDomain: "free-fire-22cac.firebaseapp.com",
+    databaseURL: "https://free-fire-22cac-default-rtdb.firebaseio.com",
+    projectId: "free-fire-22cac",
+    storageBucket: "free-fire-22cac.firebasestorage.app",
+    messagingSenderId: "554987602894",
+    appId: "1:554987602894:web:51548645a15c0d1e8d619f",
+    measurementId: "G-W2QYY1CQ8D"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
